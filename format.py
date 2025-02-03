2f85f0b3d1c5e7d0d7b81f5fd183403b18648297
Charlie Morris <cshotam@gmail.com>
2025-02-03 11:10:24 +1100

    filter out archived produts

--- format.py
+++ format.py
@@ -274,6 +274,7 @@ def main():
         print("No matching files found.")
 
     df_all = pd.read_excel(latest_file)
+    df_all = df_all[df_all["Status"] != "archived"]
 
     df_all_first_few = df_all.head(20)
 
